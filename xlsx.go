e95ea0014ae9bcd4d91a7586c149422250e1d68b
Bernardo Azevedo <bernardoazevedo.8@gmail.com>
2026-01-29 12:20:24 -0300

    feat: iniciando package xlsx2json

---
+++ xlsx.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+
+	"github.com/xuri/excelize/v2"
+)
+
+func Json(xlsReader io.Reader) (string, error) {
+	f, err := excelize.OpenReader(xlsReader)
+	if err != nil {
+		return "", fmt.Errorf("error opening file: %v", err)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			fmt.Printf("error closing file: %v", err)
+		}
+	}()
+
+	sheets := f.GetSheetList()
+	sheetName := sheets[0]
+
+	rows, err := f.Rows(sheetName)
+	if err != nil {
+		return "", fmt.Errorf("error getting rows iterator: %v", err)
+	}
+
+	rows.Next()
+	headers, err := rows.Columns()
+	if err != nil {
+		return "", fmt.Errorf("error reading headers: %v", err)
+	}
+
+	tableMap := []map[string]string{}
+	for rows.Next() {
+		rowValues, err := rows.Columns()
+		if err != nil {
+			return "", fmt.Errorf("error reading row: %v", err)
+		}
+
+		eachRow := map[string]string{}
+		for headersIndex, header := range headers {
+			if headersIndex < len(rowValues) {
+				eachRow[header] = rowValues[headersIndex]
+			} else {
+				eachRow[header] = ""
+			}
+		}
+		tableMap = append(tableMap, eachRow)
+	}
+
+	jsonSheet, err := json.Marshal(tableMap)
+	if err != nil {
+		return "", fmt.Errorf("error parsing to json: %v", err)
+	}
+
+	return string(jsonSheet), nil
+}
---
+++ xlsx_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+var tests = []struct {
+	xlsxFilepath string
+	expectedJson string
+}{
+	{"test.xlsx", `[{"headerA":"a2","headerB":"b2","headerC":"c2"},{"headerA":"a3","headerB":"b3","headerC":"c3"},{"headerA":"a4","headerB":"b4","headerC":"c4"}]`},
+}
+
+func TestJson(t *testing.T) {
+	for _, test := range tests {
+		file, err := os.Open(test.xlsxFilepath)
+		if err != nil {
+			t.Errorf("error opening %s: %v", test.xlsxFilepath, err)
+		}
+
+		json, err := Json(file)
+		if err != nil {
+			t.Errorf("error parsing %s: %v", test.xlsxFilepath, err)
+		}
+
+		if json != test.expectedJson {
+			t.Errorf("Json(%s) = %s; want %s", test.xlsxFilepath, json, test.expectedJson)
+		}
+	}
+}
+
+func BenchmarkJson(b *testing.B) {
+	test := tests[0]
+
+	file, err := os.Open(test.xlsxFilepath)
+	if err != nil {
+		fmt.Printf("error opening %s: %v", test.xlsxFilepath, err)
+	}
+	
+	for i := 0; i < b.N; i++ {
+		Json(file)
+	}
+}
